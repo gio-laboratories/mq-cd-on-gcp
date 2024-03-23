bd427c19ede0ab1cdb09365c532ac82798f04c26
galloro <galloro@google.com>
2024-03-23 10:16:34 +0000

    first commit

---
+++ createrelease.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Creating 1st release"
+echo
+###
+gcloud builds submit --substitutions=_KMS_KEY_NAME=projects/$PROJECT_ID/locations/europe-west1/keyRings/binauthz/cryptoKeys/vulnz-signer/cryptoKeyVersions/1,_KMS_DIGEST_ALG=SHA512,_NOTE_NAME=projects/$PROJECT_ID/notes/vuln-scanner-note --region=europe-west1 --project=$PROJECT_ID --config first-release.yaml
+### 
+echo "Release creation completed, please promote and approve your release manually"
